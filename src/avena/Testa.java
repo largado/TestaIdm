9763e5c37c4f32322b57c04a981c815919b4e78d
largado <Kv2940bm@>
2022-05-17 23:55:28 -0300

    Initial commit

---
+++ src/avena/Testa.java
@@ -0,0 +1,23 @@
+package avena;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+
+
+public class Testa {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient cliente = HttpClient.newBuilder().build();
+        HttpRequest requisicao = HttpRequest.newBuilder().GET()
+                .uri((URI.create("https://avena.azurewebsites.net"))).build();
+        HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());
+        System.out.println(resposta.statusCode());
+        System.out.println(resposta.body());
+
+    }
+
+}
