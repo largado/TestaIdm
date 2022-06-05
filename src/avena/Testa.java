0a4017454661b6e8cc51c33e3288fca256c9af31
largado <Kv2940bm@>
2022-06-05 17:09:21 -0300

    versão que funcionou no pod

--- src/avena/Testa.java
+++ src/avena/Testa.java
@@ -11,12 +11,16 @@ import java.net.http.HttpResponse.BodyHandlers;
 
 public class Testa {
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient cliente = HttpClient.newBuilder().build();
-        HttpRequest requisicao = HttpRequest.newBuilder().GET()
-                .uri((URI.create("https://avena.azurewebsites.net"))).build();
-        HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());
-        System.out.println(resposta.statusCode());
-        System.out.println(resposta.body());
+        while (true){
+            Thread.sleep(10000);
+            HttpClient cliente = HttpClient.newBuilder().build();
+            HttpRequest requisicao = HttpRequest.newBuilder().GET()
+                    .uri((URI.create("https://avena.azurewebsites.net"))).build();
+            HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());
+            System.out.println(resposta.statusCode());
+            System.out.println(resposta.body());
+
+        }
 
     }
 
